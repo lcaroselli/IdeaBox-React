7ad4b9520ba4581af4c841df1e1532d0fa48a303
Laura Caroselli <lcaroselli@outlook.com>
2017-08-06 15:33:53 -0600

    Add quality button images and delete button image + hover states

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default function Search (props) {
+  // function handleKeyDown(e) {
+  //   if (e.keyCode === 13) {
+  //     //props.searchIdea()
+  //   }
+  // }
+  
   return (
     <input name="search" className="search" type="search" placeholder="Search"></input>
   )
