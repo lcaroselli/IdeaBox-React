43466a6753dc2c8ef3fef6ddb963efde90938f79
Laura Caroselli <lcaroselli@outlook.com>
2017-08-07 19:12:05 -0600

    Conditionally render quality text based on state of idea

--- src/components/IdeaCard.js
+++ src/components/IdeaCard.js
@@ -1,29 +1,63 @@
 import React from 'react';
 
+let msg = 'swill'
+
 export default class IdeaCard extends React.Component {
   constructor() {
     super();
     this.state = {
       swill: true,
       plausible: false,
-      genius: false,
+      genius: false
     }
   }
 
-  updateQuality(e) {
-    // alert('it works')
+  upQuality() {
+    if(this.state.swill) {
+      this.setState ({
+        swill: false,
+        plausible: true,
+        genius: false
+      })
+    msg = <span> plausible </span>
+
+    } else if (this.state.plausible) {
+      this.setState ({
+        swill: false,
+        plausible: false,
+        genius: true
+      })
+    msg = <span> genius </span>
+
+    } else if (this.state.genius) {
+      msg =  <span> genius </span>
+    }
+  }
+
+  downQuality() {
+    if(this.state.genius) {
+      this.setState ({
+        swill: false,
+        plausible: true,
+        genius: false
+      })
+    msg = <span> plausible </span>
+
+    } else if (this.state.plausible) {
+      this.setState ({
+        swill: true,
+        plausible: false,
+        genius: false
+      })
+    msg = <span> swill </span>
+
+  } else if (this.state.swill) {
+      msg =  <span> swill </span>
+    }
   }
 
   render() {
-    var msg;
 
-    if(this.state.swill) {
-      msg = 'swill'
-    } else if (this.state.plausible) {
-      msg = 'plausible'
-    } else if (this.state.genius) {
-      msg = 'genius'
-    }
 
   return (
     <div className="idea-card">
@@ -35,17 +69,19 @@ export default class IdeaCard extends React.Component {
       </p>
       <p className="card-body"> { this.props.body }</p>
       <p className="card-quality">
-        <button
-          className="up-button"
-          onClick={ this.updateQuality.bind(this) } >
-        </button>
-        <button
-          className="down-button"
-          onClick={ this.updateQuality.bind(this) } >
-        </button> quality: { msg }
+      <button
+        className="up-button"
+        onClick={ this.upQuality.bind(this) } >
+      </button>
+      <button
+        className="down-button"
+        onClick={ this.downQuality.bind(this) } >
+      </button>
+      <p> quality: { msg } </p>
       </p>
       <hr />
     </div>
     )
   }
+
 }
